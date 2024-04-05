c84272bc53ff9f362ece3f82830df2297beab4e3
aleksis034 <152428168+aleksis034@users.noreply.github.com>
2024-04-05 14:22:38 +0800

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,1 +1,21 @@
+package mexer2.prog2.edu.slu;
 
+ class Rectangle extends Square {
+  private double side2;
+
+  public Rectangle (String name, double s1, double s2){
+   super(name,4);
+   this.side2 = s1;
+   this.side = s2;
+  }
+
+  public double area (){
+   double area = side * side2;
+   return area;
+  }
+  public double perimeter (){
+   double perimeter = (2*side)+(2*side2);
+   return perimeter;
+  }
+
+ }
