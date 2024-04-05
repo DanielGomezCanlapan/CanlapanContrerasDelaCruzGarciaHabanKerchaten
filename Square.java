c41d835185a39b6f8d6f115ff7559fa4495137ec
aleksis034 <152428168+aleksis034@users.noreply.github.com>
2024-04-05 14:21:59 +0800

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,1 +1,20 @@
+package mexer2.prog2.edu.slu;
 
+ class Square extends Shape {
+     protected double side;
+
+     public Square(String name, double side){
+         super(name,4);
+         this.side = side;
+     }
+
+     public double area (){
+         double area = side * side;
+         return area;
+     }
+     public double perimeter (){
+         double perimeter = (4*side);
+         return perimeter;
+     }
+
+}
