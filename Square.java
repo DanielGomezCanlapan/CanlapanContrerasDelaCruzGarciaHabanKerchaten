fd388d42933abec31079519e6422881ab8245808
aleksis034 <152428168+aleksis034@users.noreply.github.com>
2024-04-08 03:09:39 +0800

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,20 +1,20 @@
 package mexer2.prog2.edu.slu;
 
- class Square extends Shape {
-     protected double side;
+class Square extends Shape { // creates a class named Square that inherits from the Shape class.
+    protected double side;  // sets up a protected variable to store the length of the side of the square
 
-     public Square(String name, double side){
-         super(name,4);
-         this.side = side;
-     }
+    public Square(String name, double side) { // constructor for the Square class that initializes the name and length of the square
+        super(name, 4);  // calls the Shape superclass constructor, providing the square's name and the number of sides
+        this.side = side;   //sets the length of the side (parameter), to the instance variable 'side' 
+    } 
 
-     public double area (){
-         double area = side * side;
-         return area;
-     }
-     public double perimeter (){
-         double perimeter = (4*side);
-         return perimeter;
-     }
+    public double area() {   // method to calculate the area of the square
+        double area = side * side; // multiplying side length by itself to get the area of the square
+        return area; // returns the calculated area
+    }
 
+    public double perimeter() { // method to calculate the perimeter of the square
+         double perimeter = (4 * side); // multiplying the side length by 4 to get the perimeter of the square
+         return perimeter; // return the calculated perimeter
+    }
 }
